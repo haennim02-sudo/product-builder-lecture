697bcf83e4cf6119e2d075dd21447832d82722a0
haennim02 <haennim02@gmail.com>
2026-04-08 06:17:59 +0000

    Upload project files for product-builder-lecture

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+const generatorBtn = document.getElementById('generator-btn');
+const lottoNumbersDiv = document.getElementById('lotto-numbers');
+
+generatorBtn.addEventListener('click', () => {
+    lottoNumbersDiv.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+    
+    numbers.forEach(number => {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('lotto-number');
+        numberDiv.textContent = number;
+        lottoNumbersDiv.appendChild(numberDiv);
+    });
+});
